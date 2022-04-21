a43e10862ddf048c7b564185926063e65440c745
GonzaloBalderrama <103964019+GonzaloBalderrama@users.noreply.github.com>
2022-04-21 18:05:43 -0300

    Add files via upload

---
+++ Ejercicio 1/Email.py
@@ -0,0 +1,45 @@
+class Email:
+    __idCuenta = ''
+    __dominio = ''
+    __tipo_de_dominio = ''
+    __contra = ''
+
+    def __init__(self, idc='', dom='', tdd='', con='1234'):
+        self.__idCuenta = idc
+        self.__dominio = dom
+        self.__tipo_de_dominio = tdd
+        self.__contra = con
+        
+    def retornaEmail(self):
+        return (f'{self.__idCuenta}@{self.__dominio}.{self.__tipo_de_dominio}')
+
+    def getDominio(self):
+        return self.__dominio
+
+    def cambiarContrasenia(self):
+        ban = True
+        while ban == True:
+            contra = input("Ingrese contraña actual: ")
+            if contra == self.__contra:
+                self.__contra = input("Ingrese su nueva contraseña: ")
+                print("¡Su contraseña se actualizò correctamente!")
+                ban = False
+            else:
+                print("La contraseña ingresada no es vàlida")
+
+    def crearEmail(self):
+        ban= True
+        while ban == True: 
+            e= input("Ingrese su direcciòn de correo: ")
+            try:
+                e.index("@")
+                e.index(".")
+                self.__idCuenta = e[:e.find("@")]
+                self.__dominio = e[e.find("@")+1:e.find(".")]
+                self.__tipo_de_dominio = e[e.find(".")+1:]
+                print(self.__idCuenta)
+                print(self.__dominio)
+                print(self.__tipo_de_dominio)
+                ban = False
+            except:
+                print("¡El correo ingresado en invalido!")
