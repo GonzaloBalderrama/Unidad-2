22dcc76fb375287c1e6049dda910219e4dbf1b6c
GonzaloBalderrama <103964019+GonzaloBalderrama@users.noreply.github.com>
2022-05-09 10:24:01 -0300

    Add files via upload

---
+++ Ejercicio 6/ViajeroFrecuente.py
@@ -0,0 +1,48 @@
+class ViajeroFrecuente:
+    __num_viajero = 0
+    __dni = 0
+    __nombre = ""
+    __apellido = ""
+    __millas_acum = 0.0
+
+    def __init__(self, nv, dni, nom, ape, mac):
+        self.__num_viajero = nv
+        self.__dni = dni
+        self.__nombre = nom
+        self.__apellido = ape
+        self.__millas_acum = mac
+
+    def __str__(self):
+        return f"{self.__num_viajero}     {self.__dni}     {self.__nombre}     {self.__apellido}     {self.__millas_acum}"
+
+    def getNombre(self):
+        return self.__nombre
+
+    def getNumero(self):
+        return self.__num_viajero
+    
+    def cantidadTotaldeMillas(self):
+        return self.__millas_acum
+
+    def acumularMillas(self, cantm):
+        self.__millas_acum = self.__millas_acum+cantm
+        return self.__millas_acum
+
+    def canjearMillas(self, cantm):
+        if cantm <= self.__millas_acum:
+            self.__millas_acum -= cantm
+            return self.__millas_acum
+        else:
+            print("No cuenta con la cantidad suiciente de millas")
+
+    def __gt__(self, otroViajero):
+        ban = False
+        if self.__millas_acum>otroViajero.cantidadTotaldeMillas():
+            ban = True
+        return ban
+
+    def __add__(self, numero):
+        return self.__millas_acum+numero
+
+    def __sub__(self, numero):
+        return self.__millas_acum-numero
--- Ejercicio 6/main.py
+++ Ejercicio 6/main.py
@@ -1,2 +1,33 @@
-if __name__ == "__main__":
-    pass
+import csv
+from re import M
+from ViajeroFrecuente import ViajeroFrecuente
+
+if __name__ == '__main__':
+    lista = []
+
+    archivo = open('viajeros.csv')
+    reader = csv.reader(archivo,delimiter=';')
+    for fila in reader:
+        unViajero = ViajeroFrecuente(int(fila[0]), int(fila[1]), fila[2], fila[3], float(fila[4]))
+        lista.append(unViajero)
+        print(fila)
+    archivo.close()
+
+    print("\n****Sobrecarga del operador relacional mayor****")
+    max = 0
+    i=0
+    for fila in lista:
+        if fila>lista[max]:
+            max = i
+        i +=1
+    print(f"El viajero con mayor cantidad de millas es: {lista[max].getNombre()}")
+    
+    print("\n****Sobrecarga del operador suma****")
+    print(f"Viajero: {lista[0].getNombre()}   Cantidad de millas: {lista[0].cantidadTotaldeMillas()}")
+    numero = float(input("Ingrese la cantidad de millas a sumar: "))
+    print(f"Millas actualizadas: {lista[0]+numero}")
+
+    print("\n****Sobrecarga del operador resta****")
+    print(f"Viajero: {lista[1].getNombre()}   Cantidad de millas: {lista[1].cantidadTotaldeMillas()}")
+    numero = float(input("Ingrese la cantidad de millas a canjear: "))
+    print(f"Millas actualizadas: {lista[1]-numero}")
